be8477447beb6062060881c4ea7a5a1344ff8210
Carlos Alemany <calemanycostela@gmail.com>
2024-06-06 09:10:04 +0000

    a

--- models/marts/fct_order_items.sql
+++ models/marts/fct_order_items.sql
@@ -2,40 +2,29 @@ WITH src_order_items AS (
     SELECT *
     FROM {{ ref('stg_sql_server_dbo__order_items') }}
 ),
+
 src_products AS (
     SELECT *
     FROM {{ source('sql_server_dbo', 'products') }}
 ),
+
 renamed_casted AS (
-    SELECT
-        src_order_items.order_id,
-        src_order_items.product_id,
-        src_products.price AS product_unitary_price,
-        src_order_items._fivetran_deleted,
-        orders.shipping_service_id,
-        orders.address_id,
-        orders.created_at_utc,
-        orders.order_cost,
-        orders.order_total,
-        orders.promo_id,
-        orders.user_id,
-        CONVERT_TIMEZONE('UTC', src_order_items._fivetran_synced) AS date_load_utc,
-        COALESCE(quantity, 0) AS quantity
-    FROM src_order_items
-    LEFT JOIN src_products ON src_order_items.product_id = src_products.product_id
-    LEFT JOIN (
-        SELECT
-            shipping_service_id,
-            address_id,
-            created_at_utc,
-            order_cost,
-            order_id,
-            order_total,
-            promo_id,
-            user_id
-        FROM {{ ref('stg_sql_server_dbo__orders') }}
-    ) AS orders ON src_order_items.order_id = orders.order_id
-    WHERE COALESCE(src_order_items._fivetran_deleted, FALSE) = FALSE
+SELECT 
+    oi.order_id,
+    oi.product_id,
+    oi._fivetran_deleted,
+    o.shipping_service_id,
+    o.address_id,
+    o.created_at_utc,
+    o.order_cost,
+    o.order_total,
+    o.promo_id,
+    o.user_id,
+    CONVERT_TIMEZONE('UTC', oi._fivetran_synced) AS date_load_utc,
+    COALESCE(oi.quantity, 0) AS quantity
+FROM {{ ref('stg_sql_server_dbo__order_items') }} oi
+LEFT JOIN {{ ref('stg_sql_server_dbo__orders') }} o ON oi.order_id = o.order_id
+WHERE COALESCE(oi._fivetran_deleted, FALSE) = FALSE
 )
 
 SELECT * FROM renamed_casted
